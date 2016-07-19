112d39a07f7e506e16f7e5a3403c6482d959d0c6
Nasko <nasko@webfactory.bg>
2016-07-19 10:44:07 +0300

    use the base api controller

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -18,11 +18,11 @@ class ApiController extends BaseApiController
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index($builder = null)
     {
-        $models = $this->repository->latest(25, ['historable', 'user'], true);
-
-        return response()->json($models);
+        $builder = $this->repository->getModel()->with(['historable', 'user'])->latest();
+ 
+        return parent::index($builder);
     }
 
     /**
